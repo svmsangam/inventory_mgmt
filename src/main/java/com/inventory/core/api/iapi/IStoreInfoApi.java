6196d46799415a1d23d65aa6362f011c3b1fd828
Sanjay Hona <sanjay.hona@gmail.com>
2017-08-12 21:32:42 +0545

    store api

--- src/main/java/com/inventory/core/api/iapi/IStoreInfoApi.java
+++ src/main/java/com/inventory/core/api/iapi/IStoreInfoApi.java
@@ -1,9 +1,28 @@
 package com.inventory.core.api.iapi;
 
+import com.inventory.core.model.dto.StoreInfoDTO;
+import com.inventory.core.model.enumconstant.Status;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 /**
  * Created by dhiraj on 8/1/17.
  */
 public interface IStoreInfoApi {
 
+    StoreInfoDTO save(StoreInfoDTO storeInfoDTO);
+
+    StoreInfoDTO update(StoreInfoDTO storeInfoDTO);
+
+    void delete(long storeId);
+
+    StoreInfoDTO show(long storeId, Status status);
+
+    List<StoreInfoDTO> list(Status status);
+
+    StoreInfoDTO getStoreByNameAndStatus(String storeName, long storeId);
+
+    long storeCount(Status status, long storeId);
 
 }
--- src/main/java/com/inventory/core/model/repository/StoreInfoRepository.java
+++ src/main/java/com/inventory/core/model/repository/StoreInfoRepository.java
@@ -4,6 +4,7 @@ import com.inventory.core.model.entity.StoreInfo;
 import com.inventory.core.model.enumconstant.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,10 +17,16 @@ import java.util.List;
 @Transactional(readOnly = true)
 public interface StoreInfoRepository extends JpaRepository<StoreInfo, Long> , JpaSpecificationExecutor<StoreInfo> {
 
+    StoreInfo findById(long storeId);
+
+    @Query("select s from StoreInfo s where s.id = ?1 and s.status = ?2")
     StoreInfo findByIdAndStatus(long storeId , Status status);
 
+    @Query("select s from StoreInfo s where s.status=?1")
     List<StoreInfo> findAllByStatus(Status status);
 
     StoreInfo findByName(String storeName);
 
+    @Query("select s from StoreInfo s where s.name = ?1 and s.status = ?2")
+    StoreInfo findByNameAndStatus(String storeName, Status status);
 }
