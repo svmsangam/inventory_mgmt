2c58c42bd1284dd7ee66180067d2a704216e521b
dhirajbadu <dhirajbadu50@gmail.com>
2017-08-30 21:44:24 +0545

    orderItemInfo repository

---
+++ src/main/java/com/inventory/core/model/repository/OrderItemInfoRepository.java
@@ -0,0 +1,29 @@
+package com.inventory.core.model.repository;
+
+import com.inventory.core.model.entity.OrderItemInfo;
+import com.inventory.core.model.enumconstant.Status;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * Created by dhiraj on 8/30/17.
+ */
+@Transactional(readOnly = true)
+@Repository
+public interface OrderItemInfoRepository extends JpaRepository<OrderItemInfo , Long> , JpaSpecificationExecutor<OrderItemInfo>{
+
+    OrderItemInfo findById(long orderItemInfoId);
+
+    OrderItemInfo findByIdAndStatus(long orderItemInfoId , Status status);
+
+    @Query("select o from OrderItemInfo o where o.id = ?1 and o.status = ?2 and o.orderInfo.id = ?3")
+    OrderItemInfo findByIdAndStatusAndOrderInfo(long orderItemInfoId , Status status , long orderInfoId);
+
+    @Query("select o from OrderItemInfo o where o.status = ?1 and o.orderInfo.id = ?2")
+    List<OrderItemInfo> findAllByStatusAndOrderInfo(Status status , long orderInfoId);
+}
