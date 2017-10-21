8d776081d2b71460d97597493d43e38b12a969f6
dhirajbadu <dhirajbadu50@gmail.com>
2017-10-21 17:25:02 +0545

    updated

--- src/main/java/com/inventory/core/util/ReportGeneratorUtil.java
+++ src/main/java/com/inventory/core/util/ReportGeneratorUtil.java
@@ -20,8 +20,7 @@ import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 
-import java.awt.Color;
-
+import java.awt.*;
 import java.util.Date;
 import java.util.List;
 
@@ -29,18 +28,6 @@ import java.util.List;
  * Created by dhiraj on 10/10/17.
  */
 public class ReportGeneratorUtil {
-
-/*    public JasperPrint invoiceReport(List<TransactionDTO> listSale, String title, String subTitle) throws ColumnBuilderException, JRException, ClassNotFoundException {
-        Style headerStyle = createHeaderStyle();
-        Style detailTextStyle = createDetailTextStyle();
-        Style detailNumberStyle = createDetailNumberStyle();
-        DynamicReport dynaReport = transactionReportProcessor(title, subTitle, headerStyle, detailTextStyle, detailNumberStyle);
-        JasperPrint jp = DynamicJasperHelper.generateJasperPrint(dynaReport, new ClassicLayoutManager(),
-                new JRBeanCollectionDataSource(listSale));
-
-//		jp.setProperty("net.sf.jasperreports.awt.ignore.missing.font", "true");
-        return jp;
-    }*/
 
     public JasperPrint ledgerReport(List<LedgerInfoDTO> list, String title, String subTitle) throws ColumnBuilderException, JRException, ClassNotFoundException {
         Style headerStyle = createHeaderStyle();
@@ -74,24 +61,25 @@ public class ReportGeneratorUtil {
     private Style createDetailTextStyle() {
         StyleBuilder sb = new StyleBuilder(true);
 		/*sb.setFont(Font.VERDANA_MEDIUM);*/
-        sb.setBorder(Border.DOTTED());
+        sb.setBorder(Border.THIN());
         sb.setBorderColor(Color.BLACK);
+        sb.setStretchWithOverflow(false);
         sb.setTextColor(Color.BLACK);
         sb.setHorizontalAlign(HorizontalAlign.LEFT);
         sb.setVerticalAlign(VerticalAlign.MIDDLE);
-        sb.setPaddingLeft(5);
+        sb.setPaddingLeft(2);
         return sb.build();
     }
 
     private Style createDetailNumberStyle() {
         StyleBuilder sb = new StyleBuilder(true);
 		/*sb.setFont(Font.VERDANA_MEDIUM);*/
-        sb.setBorder(Border.DOTTED());
+        sb.setBorder(Border.THIN());
         sb.setBorderColor(Color.BLACK);
         sb.setTextColor(Color.BLACK);
         sb.setHorizontalAlign(HorizontalAlign.RIGHT);
         sb.setVerticalAlign(VerticalAlign.MIDDLE);
-        sb.setPaddingRight(5);
+        sb.setPaddingRight(4);
         sb.setPattern("#,##0.00");
         return sb.build();
     }
@@ -107,50 +95,17 @@ public class ReportGeneratorUtil {
         return columnState;
     }
 
-/*    private DynamicReport transactionReportProcessor(String title, String subTitle, Style headerStyle, Style detailTextStyle, Style detailNumStyle)
-            throws ColumnBuilderException, ClassNotFoundException {
-
-        DynamicReportBuilder report = new DynamicReportBuilder();
-
-        AbstractColumn datTime = createColumn("date", String.class, "Date and Time", 50, headerStyle, detailTextStyle);
-        AbstractColumn TransactionIde = createColumn("transactionIdentifier", String.class, "Transaction Identifier", 50, headerStyle, detailTextStyle);
-        AbstractColumn sourceUSer = createColumn("originatingUserName", String.class, "Source User", 50, headerStyle, detailNumStyle);
-        AbstractColumn destinationUser = createColumn("destinationUserName", String.class, "Destination User", 50, headerStyle, detailNumStyle);
-        AbstractColumn amount = createColumn("amount", Double.class, "Amount", 30, headerStyle, detailTextStyle);
-        AbstractColumn transactionStatus = createColumn("transactionStatus", TransactionStatus.class, "Transaction Status", 50, headerStyle, detailTextStyle);
-        AbstractColumn TransactionType = createColumn("transactionType", TransactionType.class, "Transaction Type", 50, headerStyle, detailTextStyle);
-
-        report.addColumn(datTime).addColumn(TransactionIde).addColumn(sourceUSer).addColumn(destinationUser)
-                .addColumn(amount).addColumn(transactionStatus).addColumn(TransactionType);
-
-        StyleBuilder titleStyle = new StyleBuilder(true);
-        titleStyle.setHorizontalAlign(HorizontalAlign.CENTER);
-	*//*	titleStyle.setFont(new Font(20, Font._FONT_GEORGIA, true));*//*
-
-        StyleBuilder subTitleStyle = new StyleBuilder(true);
-        subTitleStyle.setHorizontalAlign(HorizontalAlign.CENTER);
-		*//*subTitleStyle.setFont(new Font(Font.MEDIUM, Font._FONT_GEORGIA, true));*//*
-
-        report.setTitle(title);
-        report.setTitleStyle(titleStyle.build());
-        report.setSubtitle(subTitle);
-        report.setSubtitleStyle(subTitleStyle.build());
-        report.setUseFullPageWidth(true);
-        return report.build();
-    }*/
-
-
     private DynamicReport ledgerReportProcessor(String title, String subTitle, Style headerStyle, Style detailTextStyle, Style detailNumStyle)
             throws ColumnBuilderException, ClassNotFoundException {
 
         DynamicReportBuilder report = new DynamicReportBuilder();
 
-        AbstractColumn dateTime = createColumn("date", Date.class, "Date and Time", 50, headerStyle, detailTextStyle);
-        AbstractColumn accountNo = createColumn("accountNo", String.class, "Account Number", 50, headerStyle, detailTextStyle);
-        AbstractColumn amount = createColumn("amount", Double.class, "Amount", 50, headerStyle, detailNumStyle);
-        AbstractColumn ledgerEntryType = createColumn("ledgerEntryType", LedgerEntryType.class, "LedgerEntry Type", 50, headerStyle, detailNumStyle);
-        AbstractColumn accountEntryType = createColumn("accountEntryType", AccountEntryType.class, "AccountEntry Type", 30, headerStyle, detailTextStyle);
-        AbstractColumn remarks = createColumn("remarks", String.class, "Remarks", 30, headerStyle, detailTextStyle);
+        AbstractColumn dateTime = createColumn("date", Date.class, "Date and Time", 48, headerStyle, detailTextStyle);
+        AbstractColumn accountNo = createColumn("accountNo", String.class, "Account Number", 53, headerStyle, detailTextStyle);
+        AbstractColumn amount = createColumn("amount", Double.class, "Amount", 45, headerStyle, detailNumStyle);
+        AbstractColumn ledgerEntryType = createColumn("ledgerEntryType", LedgerEntryType.class, "Ledger Entry", 24, headerStyle, detailTextStyle);
+        AbstractColumn accountEntryType = createColumn("accountEntryType", AccountEntryType.class, "Account Entry", 20, headerStyle, detailTextStyle);
+        AbstractColumn remarks = createColumn("remarks", String.class, "Remarks", 68, headerStyle, detailTextStyle);
 
 
         report.addColumn(dateTime).addColumn(accountNo).addColumn(amount).addColumn(ledgerEntryType)
@@ -158,7 +113,7 @@ public class ReportGeneratorUtil {
 
         StyleBuilder titleStyle = new StyleBuilder(true);
         titleStyle.setHorizontalAlign(HorizontalAlign.CENTER);
-	/*	titleStyle.setFont(new Font(20, Font._FONT_GEORGIA, true));*/
+		/*titleStyle.setFont(new Font(20, Font._FONT_GEORGIA, true));*/
 
         StyleBuilder subTitleStyle = new StyleBuilder(true);
         subTitleStyle.setHorizontalAlign(HorizontalAlign.CENTER);
