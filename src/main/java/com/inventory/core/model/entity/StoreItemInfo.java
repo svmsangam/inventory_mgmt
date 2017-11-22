1027d00bcc081436526199b05cf515110e4e9cb8
dhirajbadu <dhirajbadu50@gmail.com>
2017-11-22 19:45:38 +0545

    first push on b2b

---
+++ src/main/java/com/inventory/core/model/entity/StoreItemInfo.java
@@ -0,0 +1,12 @@
+package com.inventory.core.model.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * Created by dhiraj on 11/22/17.
+ */
+@Entity
+@Table(name = "store_item")
+public class StoreItemInfo extends AbstractEntity<Long>{
+}
