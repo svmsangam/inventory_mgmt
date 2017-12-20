dc4d0e8add6a9c6d1ab5e9a7de172ec34f5425bf
dhirajbadu <dhirajbadu50@gmail.com>
2017-12-20 23:08:35 +0545

    updated

--- src/main/java/com/inventory/core/api/impl/ReportServiceApi.java
+++ src/main/java/com/inventory/core/api/impl/ReportServiceApi.java
@@ -100,22 +100,38 @@ class ReportServiceApi implements IReportServiceApi{
         List<OrderItemInfoDTO> orderItemList = orderItemInfoApi.getAllByStatusAndOrderInfo(Status.ACTIVE, orderInfoDTO.getOrderId());
 
         Document document = new Document();
+
+        document.setPageSize(PageSize.A4);
+
         PdfWriter.getInstance(document, new FileOutputStream(file));
+
         document.open();
-        // Left
-        Paragraph invoiceDate = new Paragraph(invoice.getInvoiceDate().toString());
+
+        Font font = new Font(Font.FontFamily.HELVETICA, 16, Font.BOLD, BaseColor.BLACK);
+
+        Paragraph storename = new Paragraph("" + invoice.getStoreInfoDTO().getName() , font);
+        storename.setAlignment(Element.ALIGN_CENTER);
+
+        document.add(storename);
+
+        Paragraph storeaddress = new Paragraph("" + invoice.getStoreInfoDTO().getCityName());
+        storeaddress.setAlignment(Element.ALIGN_CENTER);
+        document.add(storeaddress);
+
+        Paragraph storecontact = new Paragraph("" + invoice.getStoreInfoDTO().getContact());
+        storecontact.setAlignment(Element.ALIGN_CENTER);
+        document.add(storecontact);
+
+        Paragraph invoiceDate = new Paragraph("Invoice Date : " + invoice.getInvoiceDate().toString());
         invoiceDate.setAlignment(Element.ALIGN_RIGHT);
         document.add(invoiceDate);
 
-        Paragraph invoiceNo = new Paragraph(invoice.getInvoiceNo());
+        Paragraph invoiceNo = new Paragraph("Invoice No : " + invoice.getInvoiceNo());
         invoiceNo.setAlignment(Element.ALIGN_RIGHT);
         document.add(invoiceNo);
 
-        Paragraph orderNo = new Paragraph(orderInfoDTO.getOrderNo());
-        orderNo.setAlignment(Element.ALIGN_RIGHT);
-        document.add(orderNo);
-
-        Paragraph buyerDetails = new Paragraph("Buyer Details");
+        Font font1 = new Font(Font.FontFamily.HELVETICA, 14, Font.BOLD, BaseColor.BLACK);
+        Paragraph buyerDetails = new Paragraph("Buyer Details" , font1);
         buyerDetails.setAlignment(Element.ALIGN_LEFT);
         document.add(buyerDetails);
 
@@ -139,57 +155,62 @@ class ReportServiceApi implements IReportServiceApi{
         buyerEmail.setAlignment(Element.ALIGN_LEFT);
         document.add(buyerEmail);
 
-        Paragraph orderDetails = new Paragraph("Order and Delivery Details");
-        orderDetails.setAlignment(Element.ALIGN_RIGHT);
-        document.add(orderDetails);
-
-        Paragraph ordered = new Paragraph("Order Date: "+ orderInfoDTO.getOrderDate());
-        ordered.setAlignment(Element.ALIGN_RIGHT);
-        document.add(ordered);
-
-        Paragraph deliveryDate = new Paragraph("Delivery Date: " + orderInfoDTO.getDeliveryDate());
-        deliveryDate.setAlignment(Element.ALIGN_RIGHT);
-        document.add(deliveryDate);
-
-        Paragraph deliveryAddress = new Paragraph("Delivered To: "+ orderInfoDTO.getDeliveryAddress());
-        deliveryAddress.setAlignment(Element.ALIGN_RIGHT);
-        document.add(deliveryAddress);
-
-        Paragraph receivableAmount = new Paragraph("Receivalble Amount: " + invoice.getReceivableAmount());
-        receivableAmount.setAlignment(Element.ALIGN_RIGHT);
-        document.add(receivableAmount);
-
-
         PdfPTable table = new PdfPTable(5);
-        // the cell object
+        table.setSpacingBefore(15);
+        table.setSpacingAfter(10);
+        table.setWidthPercentage(530 / 5.23f);
+
         PdfPCell cell01 = new PdfPCell(new Phrase("Product"));
+        cell01.setBackgroundColor(BaseColor.GRAY);
+        cell01.setMinimumHeight(30);
+        cell01.setPaddingLeft(10);
         table.addCell(cell01);
 
         PdfPCell cell02 = new PdfPCell(new Phrase("Quantity"));
+        cell02.setBackgroundColor(BaseColor.GRAY);
+        cell02.setMinimumHeight(30);
+        cell02.setPaddingLeft(10);
         table.addCell(cell02);
 
         PdfPCell cell03 =  new PdfPCell(new Phrase("Rate"));
+        cell03.setBackgroundColor(BaseColor.GRAY);
+        cell03.setMinimumHeight(30);
+        cell03.setPaddingLeft(10);
         table.addCell(cell03);
 
         PdfPCell cell04 = new PdfPCell(new Phrase("Discount"));
+        cell04.setBackgroundColor(BaseColor.GRAY);
+        cell04.setMinimumHeight(30);
+        cell04.setPaddingLeft(10);
         table.addCell(cell04);
 
         PdfPCell cell05 = new PdfPCell(new Phrase("Total"));
+        cell05.setBackgroundColor(BaseColor.GRAY);
+        cell05.setMinimumHeight(30);
+        cell05.setPaddingLeft(10);
         table.addCell(cell05);
 
         for (OrderItemInfoDTO orderItemDTO : orderItemList) {
 
 
             PdfPCell cell1 = new PdfPCell(new Phrase(orderItemDTO.getItemInfoDTO().getProductInfo().getName()));
+            cell1.setMinimumHeight(20);
+            cell1.setPaddingLeft(5);
             table.addCell(cell1);
 
             PdfPCell cell2 = new PdfPCell(new Phrase(String.valueOf(orderItemDTO.getQuantity())));
+            cell2.setMinimumHeight(20);
+            cell2.setPaddingLeft(5);
             table.addCell(cell2);
 
             PdfPCell cell3 = new PdfPCell(new Phrase(String.valueOf(orderItemDTO.getRate())));
+            cell3.setMinimumHeight(20);
+            cell3.setPaddingLeft(5);
             table.addCell(cell3);
 
             PdfPCell cell4=  new PdfPCell(new Phrase(String.valueOf(orderItemDTO.getDiscount())));
+            cell4.setMinimumHeight(20);
+            cell4.setPaddingLeft(5);
             table.addCell(cell4);
 
             double amount = orderItemDTO.getRate() * orderItemDTO.getQuantity();
@@ -197,14 +218,64 @@ class ReportServiceApi implements IReportServiceApi{
             amount = amount - (amount * (orderItemDTO.getDiscount() /100));
 
             PdfPCell cell5=  new PdfPCell(new Phrase(String.valueOf(amount)));
+            cell5.setMinimumHeight(20);
+            cell5.setPaddingLeft(5);
             table.addCell(cell5);
         }
 
 
+        PdfPCell cell4=  new PdfPCell(new Phrase("Total : "));
+        cell4.setColspan(4);
+        cell4.setPaddingLeft(320);
+        cell4.setBackgroundColor(BaseColor.LIGHT_GRAY);
+        cell4.setMinimumHeight(20);
+        table.addCell(cell4);
+
+        PdfPCell cell5=  new PdfPCell(new Phrase("" + invoice.getOrderInfo().getTotalAmount()));
+        cell5.setMinimumHeight(20);
+        cell5.setBackgroundColor(BaseColor.LIGHT_GRAY);
+        table.addCell(cell5);
+
+
+        PdfPCell cell9 =  new PdfPCell(new Phrase("Tax(%) : "));
+        cell9.setColspan(4);
+        cell9.setPaddingLeft(320);
+        cell9.setBackgroundColor(BaseColor.LIGHT_GRAY);
+        cell9.setMinimumHeight(20);
+        table.addCell(cell9);
+
+        PdfPCell cell10=  new PdfPCell(new Phrase("" + invoice.getOrderInfo().getTax()));
+        cell10.setMinimumHeight(20);
+        cell10.setBackgroundColor(BaseColor.LIGHT_GRAY);
+        table.addCell(cell10);
+
+
+        PdfPCell cell14=  new PdfPCell(new Phrase("Grand Total : "));
+        cell14.setColspan(4);
+        cell14.setPaddingLeft(320);
+        cell14.setBackgroundColor(BaseColor.LIGHT_GRAY);
+        cell14.setMinimumHeight(20);
+        table.addCell(cell14);
+
+        PdfPCell cell15=  new PdfPCell(new Phrase("" + invoice.getTotalAmount()));
+        cell15.setMinimumHeight(20);
+        cell15.setBackgroundColor(BaseColor.LIGHT_GRAY);
+        table.addCell(cell15);
+
+        PdfPCell cell19=  new PdfPCell(new Phrase("Entered By : "));
+        cell19.setColspan(4);
+        cell19.setPaddingLeft(320);
+        cell19.setBackgroundColor(BaseColor.LIGHT_GRAY);
+        cell19.setMinimumHeight(20);
+        table.addCell(cell19);
+
+        PdfPCell cell20=  new PdfPCell(new Phrase(invoice.getCreatedByName()));
+        cell20.setMinimumHeight(20);
+        cell20.setBackgroundColor(BaseColor.LIGHT_GRAY);
+        table.addCell(cell20);
+
         document.add(table);
-        Paragraph enteredBy = new Paragraph(invoice.getCreatedByName());
-        enteredBy.setAlignment(Element.ALIGN_RIGHT);
-        document.add(enteredBy);
+
         document.close();
 
         return file;
