f098ad97638d82e600585793f9cc4fe300eeff21
dhirajbadu <dhirajbadu50@gmail.com>
2017-12-23 12:29:25 +0545

    updated

--- src/main/java/com/inventory/core/api/impl/XLSReport.java
+++ src/main/java/com/inventory/core/api/impl/XLSReport.java
@@ -118,6 +118,100 @@ public class XLSReport extends AbstractXlsxView {
 
         //buyer info end
 
+
+        //order item info start
+
+        Row row = sheet.createRow(13);
+
+        Cell cellProductHeader = row.createCell(1);
+        cellProductHeader.setCellValue("Product");
+
+        Cell cellQuantityHeader = row.createCell(2);
+        cellQuantityHeader.setCellValue("Quantity");
+
+        Cell cellRateHeader = row.createCell(3);
+        cellRateHeader.setCellValue("Rate");
+
+        Cell cellDiscountHeader = row.createCell(4);
+        cellDiscountHeader.setCellValue("Discount(%)");
+
+        Cell cellTotalHeader = row.createCell(5);
+        cellTotalHeader.setCellValue("Total");
+
+        int count = 14;
+
+        for (OrderItemInfoDTO orderItemDTO : orderItemList) {
+
+            Row rownew = sheet.createRow(count);
+
+            Cell cellProduct = rownew.createCell(1);
+            cellProduct.setCellValue(orderItemDTO.getItemInfoDTO().getProductInfo().getName());
+
+            Cell cellQuantity = rownew.createCell(2);
+            cellQuantity.setCellValue(orderItemDTO.getQuantity());
+
+            Cell cellRate = rownew.createCell(3);
+            cellRate.setCellValue(orderItemDTO.getRate());
+
+            Cell cellDiscount = rownew.createCell(4);
+            cellDiscount.setCellValue(orderItemDTO.getDiscount());
+
+            double amount = orderItemDTO.getRate() * orderItemDTO.getQuantity();
+
+            amount = amount - (amount * (orderItemDTO.getDiscount() / 100));
+
+            Cell cellTotal = rownew.createCell(5);
+            cellTotal.setCellValue(amount);
+
+            count ++;
+
+        }
+
+        //order item info end
+
+
+        //total amount start
+
+        count = count +1;
+
+        Row rowTotal = sheet.createRow(count);
+
+        Cell cellSubTotalHeader = rowTotal.createCell(4);
+        cellSubTotalHeader.setCellValue("SubTotal");
+        Cell cellSubTotal = rowTotal.createCell(5);
+        cellSubTotal.setCellValue(invoice.getOrderInfo().getTotalAmount());
+
+        count = count +1;
+
+        Row rowTax = sheet.createRow(count);
+
+        Cell cellTaxHeader = rowTax.createCell(4);
+        cellTaxHeader.setCellValue("Tax(%)");
+        Cell cellTax = rowTax.createCell(5);
+        cellTax.setCellValue(invoice.getOrderInfo().getTax());
+
+        count = count +1;
+
+        Row rowGrandTotal = sheet.createRow(count);
+
+        Cell cellGrandTotalHeader = rowGrandTotal.createCell(4);
+        cellGrandTotalHeader.setCellValue("Grand Total");
+        Cell cellGrandTotal = rowGrandTotal.createCell(5);
+        cellGrandTotal.setCellValue(invoice.getTotalAmount());
+
+        count = count +1;
+
+        Row rowEnteredBy = sheet.createRow(count);
+
+        Cell cellEnteredByHeader = rowEnteredBy.createCell(4);
+        cellEnteredByHeader.setCellValue("Entered By");
+        Cell cellEnteredBy = rowEnteredBy.createCell(5);
+        cellEnteredBy.setCellValue(invoice.getCreatedByName());
+
+
+
+        //total amount end
+
         renderWorkbook(workbook , response);
 
 
